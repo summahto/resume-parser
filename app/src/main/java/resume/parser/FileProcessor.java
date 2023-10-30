bc29634cfb06a548727aed33c13967e8c98db571
summahto <sumitkumarmahto3@gmail.com>
2023-10-30 17:18:49 -0400

    initial commit

---
+++ app/src/main/java/resume/parser/FileProcessor.java
@@ -0,0 +1,19 @@
+package resume.parser;
+
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+public class FileProcessor implements Callable<Boolean> {
+
+    private Path path;
+
+    public FileProcessor(Path path) {
+        this.path = path;
+    }
+
+    @Override
+    public Boolean call() throws Exception {
+        throw new UnsupportedOperationException("Unimplemented method 'call'");
+    }
+
+}
